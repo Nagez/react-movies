5fbcbf7d814373bd73a9f7de83a9da24ab4ac4d5
Nagez <81879748+Nagez@users.noreply.github.com>
2023-11-03 13:33:19 +0200

    added .env and axios for genreIndex

--- src/Genres/IndexGenres.tsx
+++ src/Genres/IndexGenres.tsx
@@ -1,6 +1,17 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { genreDTO } from "./genres.model";
+import {urlGenres} from "../endpoints";
 
 export default function IndexGenres(){
+
+    useEffect(()=>{
+        axios.get(urlGenres)
+            .then((reponse:AxiosResponse<genreDTO[]>)=> {console.log(reponse.data);console.log(urlGenres);
+        })
+    },[])
+
     return(
         <>
             <h3>Genres index</h3>
